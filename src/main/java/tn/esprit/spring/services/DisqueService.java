31e638f1826394aed1340ba078592ff606baabde
IbtissemBelkhir <ibtissem.belkhir@esprit.tn>
2022-03-25 21:38:20 +0100

    resolve conflits

--- src/main/java/tn/esprit/spring/services/DisqueService.java
+++ src/main/java/tn/esprit/spring/services/DisqueService.java
@@ -1,26 +1,22 @@
 package tn.esprit.spring.services;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
-<<<<<<< HEAD
+
 import tn.esprit.spring.entities.Disque;
-=======
+
 import tn.esprit.spring.entities.DataCenter;
-import tn.esprit.spring.entities.Disque;
+
 import tn.esprit.spring.repositories.IDataCenterRepository;
->>>>>>> 47d7bdae18ef243494cb7f49ff83b2581caf1e04
+
 import tn.esprit.spring.repositories.IDisqueRepository;
-
-
 
 public class DisqueService implements IDisqueService {
 
 	@Autowired
 	IDisqueRepository disqueRepository;
-<<<<<<< HEAD
-=======
+
 	IDataCenterRepository datacenterRepository  ;
->>>>>>> 47d7bdae18ef243494cb7f49ff83b2581caf1e04
+
 	
 	@Override
 	public void ajouterDisque(Disque disque) {
@@ -33,8 +29,7 @@ public class DisqueService implements IDisqueService {
 		
 	}
 
-<<<<<<< HEAD
-=======
+
 	@Override
 	public void affecterDisqueDatacenter(int idD, int idDc) {
 		Disque ds = disqueRepository.findById(idD).orElse(null);
@@ -43,5 +38,5 @@ public class DisqueService implements IDisqueService {
 		datacenterRepository.save(dc);
 	}
 
->>>>>>> 47d7bdae18ef243494cb7f49ff83b2581caf1e04
+
 }
